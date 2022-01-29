73a3caa413dcaaa26aced92f7026f7ca559fb9a3
Masha Gorbunova <masha.gorbunova@gmail.com>
2022-01-29 21:55:06 +0300

    add header text

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -6,6 +6,7 @@ import {Nav} from "../Nav/Nav";
 export const Header = () => {
     return (
         <div className={s.header}>
+            <h1 className={s.headerText}>Welcome to my crib</h1>
             <Nav/>
         </div>
     )
